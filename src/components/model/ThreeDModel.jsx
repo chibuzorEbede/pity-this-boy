77cfd8ea5e69b10ea47328b1d398206696f789ab
Chibuzor Ebede <chibuzorebede@gmail.com>
2025-05-27 15:07:03 +0100

    add delay to the aniamation

--- src/components/model/ThreeDModel.jsx
+++ src/components/model/ThreeDModel.jsx
@@ -3,28 +3,33 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import styles from "./Model.module.css";
 import cx from "classnames";
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 
 function Model({ url }) {
   const { scene } = useGLTF(url);
-  return <primitive object={scene} scale={[3.6, 3.6, 3.6]} />;
+  return <primitive object={scene} scale={[4, 4, 4]} />;
 }
 
 const ThreeDModel = () => {
   return (
-    <div className={cx(styles["canvas-container"])}>
-      <Canvas>
-        <ambientLight intensity={2} />
-        <pointLight intensity={1} />
-        <Model url="planet_saturn.glb" />
-        <OrbitControls
-          makeDefault
-          minDistance={5}
-          maxDistance={20}
-          initialPosition={[0, 0, 0]}
-        />
-      </Canvas>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        transition={{ delay: 5 }}
+        className={cx(styles["canvas-container"])}
+      >
+        <Canvas>
+          <ambientLight intensity={3} />
+          <pointLight intensity={1} />
+          <Model url="planet_saturn.glb" />
+          <OrbitControls
+            makeDefault
+            minDistance={5}
+            maxDistance={20}
+            initialPosition={[0, 0, 0]}
+          />
+        </Canvas>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
